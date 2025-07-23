5aecb5570a2b9fd7db903e705b67050e77dc0b8e
Saurav <sauravchauhan652003@gmail.com>
2025-07-23 13:37:35 +0530

    Added getters and setters

--- src/main/java/com/example/Food/Delivery/App/Backend/OrderManagement/Address.java
+++ src/main/java/com/example/Food/Delivery/App/Backend/OrderManagement/Address.java
@@ -9,6 +9,63 @@ public class Address {
     private String city;
     private String pincode;
     private String state;
+
+    public String getLine1() {
+        return line1;
+    }
+
+    public void setLine1(String line1) {
+        this.line1 = line1;
+    }
+
+    public String getLine2() {
+        return line2;
+    }
+
+    public void setLine2(String line2) {
+        this.line2 = line2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPincode() {
+        return pincode;
+    }
+
+    public void setPincode(String pincode) {
+        this.pincode = pincode;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
     private Double latitude;
     private Double longitude;
 }
--- src/main/java/com/example/Food/Delivery/App/Backend/OrderManagement/OrderItem.java
+++ src/main/java/com/example/Food/Delivery/App/Backend/OrderManagement/OrderItem.java
@@ -10,6 +10,55 @@ public class OrderItem {
     private double price;
     private int quantity;
     private double totalprice;
+
+    public String getItemname() {
+        return itemname;
+    }
+
+    public void setItemname(String itemname) {
+        this.itemname = itemname;
+    }
+
+    public MenuTime getMenuTime() {
+        return menuTime;
+    }
+
+    public void setMenuTime(MenuTime menuTime) {
+        this.menuTime = menuTime;
+    }
+
+    public FoodType getFoodType() {
+        return foodType;
+    }
+
+    public void setFoodType(FoodType foodType) {
+        this.foodType = foodType;
+    }
+
+    public double getTotalprice() {
+        return totalprice;
+    }
+
+    public void setTotalprice(double totalprice) {
+        this.totalprice = totalprice;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
     private FoodType foodType;
     private MenuTime menuTime;
 }
